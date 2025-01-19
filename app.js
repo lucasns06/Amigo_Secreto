ed974abf1f12cd65f7cecc06ce29dbc08bb04260
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-01-19 19:46:11 -0300

    Funcionalidade adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomeAmigo = document.querySelector(".input-name").value;
+    
+    if (nomeAmigo.trim() === "") {
+        alert("Por favor, insira um nome válido!");
+        return;
+    }
+
+    amigos.push(nomeAmigo)
+
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = amigos.join("<br/>");
+
+    document.querySelector(".input-name").value = "";
+}
